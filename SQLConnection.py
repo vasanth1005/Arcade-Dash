bb078aaffdfe5249773168b3381a2db21cdbad91
Vasanth <vasanthkganesan@gmail.com>
2024-05-01 17:54:39 +0100

    Update SQLConnection.py
    removing sensitive database information from public repo

--- SQLConnection.py
+++ SQLConnection.py
@@ -1,7 +1,7 @@
 import mysql.connector as sql
 
 def Connection():
-    return sql.connect(user="elqit261i8y6iibrs5hq",
-                        password="pscale_pw_yM3r8gtq2ljyGhZOSqWodhw6v6lCMB5PDLP6gGdWcrz",
+    return sql.connect(user="****",
+                        password="*****",
                         host="us-east.connect.psdb.cloud",
                         database="pythongames")
